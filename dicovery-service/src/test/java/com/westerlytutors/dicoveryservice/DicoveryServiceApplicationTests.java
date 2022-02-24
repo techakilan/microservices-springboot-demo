19e4e72f0b4d221112f3acac2d46f516514b13a4
Akilan T <techakilan@gmail.com>
2022-02-24 16:53:34 +0530

    basic setup 3 simple microservices+dicscovery service eureka

---
+++ dicovery-service/src/test/java/com/westerlytutors/dicoveryservice/DicoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.westerlytutors.dicoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DicoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ movie-catalog-service/src/main/java/com/westerlytutors/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,20 @@
+package com.westerlytutors.moviecatalogservice.resources;
+
+import java.util.Collections;
+import java.util.List;
+
+import com.westerlytutors.moviecatalogservice.models.CatalogItem;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+        return Collections.singletonList(
+                new CatalogItem("Transformers", "Test", 4));
+    }
+}
