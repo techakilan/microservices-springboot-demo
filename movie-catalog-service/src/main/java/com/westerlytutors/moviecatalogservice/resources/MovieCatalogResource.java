5b718a65534dde87872c88b6314f176a60efeb1b
Akilan T <techakilan@gmail.com>
2022-02-25 20:26:52 +0530

    added resilience4j circuit breaker

--- movie-catalog-service/src/main/java/com/westerlytutors/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/westerlytutors/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,10 +1,11 @@
 package com.westerlytutors.moviecatalogservice.resources;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import com.westerlytutors.moviecatalogservice.models.CatalogItem;
-import com.westerlytutors.moviecatalogservice.models    .Movie;
+import com.westerlytutors.moviecatalogservice.models.Movie;
 import com.westerlytutors.moviecatalogservice.models.UserRating;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,13 +14,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
+    private static final String CATALOG_SERVICE = "catalogService";
     @Autowired
     RestTemplate restTemplate;
 
     @RequestMapping("/{userId}")
+    @CircuitBreaker(name = CATALOG_SERVICE, fallbackMethod = "catalogFallback")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
         UserRating userRating = restTemplate.getForObject("http://ratings-data-service/ratingsdata/user/" + userId,
                 UserRating.class);
@@ -30,5 +35,10 @@ public class MovieCatalogResource {
                             "http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
                     return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
                 }).collect(Collectors.toList());
+    }
+
+    public List<CatalogItem> catalogFallback(Exception ex) {
+        return Arrays.asList(new CatalogItem("No Movie", "No desc", 0));    
+
     }
 }
