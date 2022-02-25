140fe71d3d0eb92bf6b4ee26f26075ebe1162629
Akilan T <techakilan@gmail.com>
2022-02-25 17:51:38 +0530

    Consuming external REST Api

--- movie-info-service/src/main/java/com/westerlytutors/movieinfoservice/resources/MovieResource.java
+++ movie-info-service/src/main/java/com/westerlytutors/movieinfoservice/resources/MovieResource.java
@@ -1,26 +1,31 @@
 package com.westerlytutors.movieinfoservice.resources;
 
 import com.westerlytutors.movieinfoservice.models.Movie;
+import com.westerlytutors.movieinfoservice.models.MovieSummary;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/movies")
 public class MovieResource {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Value("${api.key}")
+    private String apiKey;
+
     @RequestMapping("/{movieId}")
     public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
-        Movie movie;
-        if(movieId.equals("100")){
-            movie = new Movie(movieId, "Avengers", "Marvel movie");
-        }
-        else if(movieId.equals("200")) {
-            movie = new Movie(movieId, "Terminator", "Arnold movie");
-        }
-        else {
-            movie = new Movie(movieId, "No movie found", "");
-        }
-        return movie;
+
+        MovieSummary movieSummary = restTemplate.getForObject(
+                "https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" + apiKey, MovieSummary.class);
+        return new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
+
     }
 }
