368aa31153007bdcfce5780d9453f6aa6107d1be
Akilan T <techakilan@gmail.com>
2022-02-27 04:47:57 +0530

    dynamic config refresh using actuator refresh hook

--- movie-catalog-service/src/main/java/com/westerlytutors/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/westerlytutors/moviecatalogservice/resources/MovieCatalogResource.java
@@ -10,6 +10,7 @@ import com.westerlytutors.moviecatalogservice.models.UserRating;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @RestController
 @RequestMapping("/catalog")
+@RefreshScope
 public class MovieCatalogResource {
     private static final String CATALOG_SERVICE = "catalogService";
     @Autowired
