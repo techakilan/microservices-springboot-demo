6cfd2db177b0d824c820b5828a8d943ef1f276c6
Akilan T <techakilan@gmail.com>
2022-03-06 22:12:40 +0530

    Added api gateway as eureka client and added route for catalog service

--- microservices-api-gateway/src/main/java/com/westerlytutors/microservicesapigateway/MicroservicesApiGatewayApplication.java
+++ microservices-api-gateway/src/main/java/com/westerlytutors/microservicesapigateway/MicroservicesApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.westerlytutors.microservicesapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroservicesApiGatewayApplication {
 
 	public static void main(String[] args) {
