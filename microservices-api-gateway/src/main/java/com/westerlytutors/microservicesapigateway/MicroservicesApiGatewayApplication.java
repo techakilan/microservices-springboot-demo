470f5c8d1138551aede6de65f1cf81b7ae41a034
Akilan T <techakilan@gmail.com>
2022-03-06 21:04:42 +0530

    Added basic api gateway with route to movie info service

---
+++ microservices-api-gateway/src/main/java/com/westerlytutors/microservicesapigateway/MicroservicesApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.westerlytutors.microservicesapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesApiGatewayApplication.class, args);
+	}
+
+}
---
+++ microservices-api-gateway/src/test/java/com/westerlytutors/microservicesapigateway/MicroservicesApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.westerlytutors.microservicesapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
