00645394792578b8fd191650e24652fc7df9465a
Akilan T <techakilan@gmail.com>
2022-04-09 18:34:42 +0530

    simple bug fix in catalog service

--- movie-catalog-service/src/main/java/com/westerlytutors/moviecatalogservice/models/Movie.java
+++ movie-catalog-service/src/main/java/com/westerlytutors/moviecatalogservice/models/Movie.java
@@ -1,41 +1,43 @@
 package com.westerlytutors.moviecatalogservice.models;
 
+
+
 public class Movie {
-    private String movieId;
+    private int movieId;
     private String name;
-    private String description;
-
-    public Movie() {
-
-    }
-
-    public Movie(String movieId, String name, String description) {
+    private String description; 
+    private   String movieInfoServerUrl;
+    public Movie(int movieId, String name, String description, String movieInfoServerUrl) {
         this.movieId = movieId;
         this.name = name;
         this.description = description;
+        this.movieInfoServerUrl = movieInfoServerUrl;
+    }   
+    public Movie() {
     }
-
-    public String getMovieId() {
-        return movieId;
-    }
-
-    public void setMovieId(String movieId) {
-        this.movieId = movieId;
-    }
-
+   
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getDescription() {
         return description;
     }
-
     public void setDescription(String description) {
         this.description = description;
+    }
+    public int getMovieId() {
+        return movieId;
+    }
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+    public String getMovieInfoServerUrl() {
+        return movieInfoServerUrl;
+    }
+    public void setMovieInfoServerUrl(String movieInfoServerUrl) {
+        this.movieInfoServerUrl = movieInfoServerUrl;
     }
 }
